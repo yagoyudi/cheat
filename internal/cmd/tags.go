72f56bfbcc9f9b29fb552007f8bd81db6329e215
yyvf <yyvf@pm.me>
2025-03-23 19:46:32 -0300

    feat: tags cmd

--- internal/cmd/tags.go
+++ internal/cmd/tags.go
@@ -2,29 +2,39 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/yagoyudi/cheat/internal/config"
 	"github.com/yagoyudi/cheat/internal/display"
 	"github.com/yagoyudi/cheat/internal/sheets"
 )
 
-// cmdTags lists all tags in use.
-func Tags(_ map[string]interface{}, conf config.Config) {
+var tagsCmd = &cobra.Command{
+	Use:     `tags`,
+	Short:   `Lists all tags in use`,
+	Example: `  cheat tags`,
+	RunE: func(_ *cobra.Command, _ []string) error {
+		var conf config.Config
+		if err := viper.Unmarshal(&conf); err != nil {
+			return err
+		}
 
-	// load the cheatsheets
-	cheatsheets, err := sheets.Load(conf.Cheatpaths)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to list cheatsheets: %v\n", err)
-		os.Exit(1)
-	}
+		// load the cheatsheets
+		cheatsheets, err := sheets.Load(conf.Cheatpaths)
+		if err != nil {
+			return err
+		}
 
-	// assemble the output
-	out := ""
-	for _, tag := range sheets.Tags(cheatsheets) {
-		out += fmt.Sprintln(tag)
-	}
+		// assemble the output
+		out := ""
+		for _, tag := range sheets.Tags(cheatsheets) {
+			out += fmt.Sprintln(tag)
+		}
 
-	// display the output
-	display.Write(out, conf)
+		// display the output
+		display.Write(out, conf)
+
+		return nil
+	},
 }
