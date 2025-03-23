c494e2ccd3be72e709a93fe9104e13a21c5f788d
yyvf <yyvf@pm.me>
2025-03-23 20:18:48 -0300

    refactor: move setup from rootCmd to initCmd

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/yagoyudi/cheat/internal/installer"
 )
 
 func init() {
@@ -19,42 +18,8 @@ func init() {
 		os.Exit(1)
 	}
 	configPath := filepath.Join(home, ".config", "cheat")
-	configFile := filepath.Join(configPath, "config.yaml")
 	viper.AddConfigPath(configPath)
 	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// prompt the user to create a config file
-			yes, err := installer.Prompt(
-				"A config file was not found. Would you like to create one now? [Y/n]",
-				true,
-			)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "cheat: cmd: %v\n", err)
-				os.Exit(1)
-			}
-
-			// exit early on a negative answer
-			if !yes {
-				os.Exit(0)
-			}
-
-			// run the installer
-			if err := installer.Run(configTemplate, configFile); err != nil {
-				fmt.Fprintf(os.Stderr, "cheat: cmd: %v\n", err)
-				os.Exit(1)
-			}
-
-			// notify the user and exit
-			fmt.Printf("Created config file: %s\n", configFile)
-			fmt.Println("Please read this file for advanced configuration information.")
-			fmt.Println()
-		} else {
-			fmt.Fprintf(os.Stderr, "cheat: cmd: %v\n", err)
-			os.Exit(1)
-		}
-	}
 
 	rootCmd.AddCommand(
 		listCmd,
--- internal/cmd/init.go
+++ internal/cmd/init.go
@@ -4,56 +4,56 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
-	"strings"
 
 	"github.com/spf13/cobra"
-	"github.com/yagoyudi/cheat/internal/config"
+	"github.com/spf13/viper"
+	"github.com/yagoyudi/cheat/internal/installer"
 )
 
 var initCmd = &cobra.Command{
 	Use:     "init",
 	Args:    cobra.ExactArgs(0),
-	Short:   "Write a default config file to stdout",
+	Short:   `Setup cheat`,
 	Example: "  cheat init",
 	RunE: func(cmd *cobra.Command, _ []string) error {
 		home, err := os.UserHomeDir()
 		if err != nil {
-			return err
+			fmt.Fprintf(os.Stderr, "cheat: cmd: %v", err)
+			os.Exit(1)
 		}
-
-		// read the envvars into a map of strings
-		envvars := map[string]string{}
-		for _, e := range os.Environ() {
-			pair := strings.SplitN(e, "=", 2)
-			envvars[pair[0]] = pair[1]
-		}
-
-		// load the config template
-		configs := configTemplate
-
-		// identify the os-specifc paths at which configs may be located
-		confpaths, err := config.Paths(runtime.GOOS, home, envvars)
+		configPath := filepath.Join(home, ".config", "cheat")
+		configFile := filepath.Join(configPath, "config.yaml")
+		err = viper.ReadInConfig()
 		if err != nil {
-			return err
+			_, ok := err.(viper.ConfigFileNotFoundError)
+			if !ok {
+				return err
+			}
+			// prompt the user to create a config file
+			yes, err := installer.Prompt(
+				"A config file was not found. Would you like to create one now? [Y/n]",
+				true,
+			)
+			if err != nil {
+				return err
+			}
+
+			// exit early on a negative answer
+			if !yes {
+				os.Exit(0)
+			}
+
+			// run the installer
+			if err := installer.Run(configTemplate, configFile); err != nil {
+				return err
+			}
+
+			// notify the user and exit
+			fmt.Printf("Created config file: %s\n", configFile)
+			fmt.Println("Please read this file for advanced configuration information.")
+			fmt.Println()
 		}
-
-		// determine the appropriate paths for config data and (optional) community
-		// cheatsheets based on the user's platform
-		confpath := confpaths[0]
-		confdir := filepath.Dir(confpath)
-
-		// create paths for community and personal cheatsheets
-		community := filepath.Join(confdir, "cheatsheets", "community")
-		personal := filepath.Join(confdir, "cheatsheets", "personal")
-
-		// template the above paths into the default configs
-		configs = strings.Replace(configs, "COMMUNITY_PATH", community, -1)
-		configs = strings.Replace(configs, "PERSONAL_PATH", personal, -1)
-
-		// output the templated configs
-		fmt.Println(configs)
-
+		fmt.Println(`All good to go!`)
 		return nil
 	},
 }
